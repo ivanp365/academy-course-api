6f558852be4d19d1c158420e9edec1e1529af71a
Ivan <pardoivan365@gmail.com>
2026-03-25 08:10:56 -0500

    Punto 2: Course entity and repository - Table created by Hibernate

---
+++ OneDrive/Desktop/Electivas/Desarrolo web/course-api/course-api/src/main/java/com/CIPP/course_api/model/Course.java
@@ -0,0 +1,26 @@
+package com.CIPP.course_api.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "course")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 150)
+    private String name;
+
+    @Column(length = 500)
+    private String description;
+}
---
+++ OneDrive/Desktop/Electivas/Desarrolo web/course-api/course-api/src/main/java/com/CIPP/course_api/repository/CourseRepository.java
@@ -0,0 +1,9 @@
+package com.CIPP.course_api.repository;
+
+import com.CIPP.course_api.model.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+}
